4c17f75eea5b80de75699370d63d89eff424b917
heesak <adesuyiojo04@gmail.com>
2022-10-15 19:48:39 +0100

    final

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var hamburger = document.querySelector('.hamburger')
+var mobilelink = document.querySelector('.mobile-links')
+var backdrop = document.querySelector('.backdrop')
+
 function Addclass() {
     hamburger.classList.toggle('show')
+    mobilelink.classList.toggle('showlinks')
+    backdrop.classList.toggle('showlinks')
 }
